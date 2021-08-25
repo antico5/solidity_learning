af641dc57695be097503104a5df23743f6ff79a9
Armando Andini <armando.andini@gmail.com>
2021-08-25 14:51:55 -0300

    More learning

--- faucet/faucet.sol
+++ faucet/faucet.sol
@@ -1,10 +1,38 @@
 pragma solidity ^0.8.7;
 
-contract Faucet {
-    function withdraw(uint256 amt) public {
-        require(amt < 10000000000000000000000);
-        payable(msg.sender).transfer(amt);
+contract owned {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
     }
 
-    receive() external payable {}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+}
+
+contract mortal is owned {
+    function kill() public onlyOwner {
+        selfdestruct(payable(owner));
+    }
+}
+
+contract receiver {
+    event Deposit(address indexed from, uint256 amount);
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+}
+
+contract Faucet is mortal, receiver {
+    event Withdrawal(address indexed to, uint256 amount);
+
+    function withdraw(uint256 amt) public {
+        require(amt <= 0.1 ether, "can only withdraw less than 0.1");
+        payable(msg.sender).transfer(amt);
+        emit Withdrawal(msg.sender, amt);
+    }
 }
